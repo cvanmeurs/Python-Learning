55741a91afb28f8b1791a1da22acdcb7c70370cb
Christian Van Meurs <cvanmeurs@mozilla.com>
2021-05-06 15:27:36 -0800

    Generate a random number between 1 and 100

---
+++ randomNumber1-100.py
@@ -0,0 +1,13 @@
+#print a random number between 1 and 100
+
+import random #load the random function
+
+randNum = random.randint(0, 100) #assign a random number between 1 and 100 to a variable
+
+print(randNum) #print the variable
+
+numberList = []
+
+numberList.append(randNum)
+
+print(numberList)
