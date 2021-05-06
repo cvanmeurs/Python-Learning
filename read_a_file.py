c6482d2cd4767a3d2ea56f634b835c6bc8e7c396
John Shaughnessy <johnfshaughnessy@gmail.com>
2021-05-06 11:08:19 -0700

    Add program to read a file

---
+++ read_a_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Define a filename.
+filename = "numbers.txt"
+
+# Open the file as f.
+# The function readlines() reads the file.
+with open(filename) as f:
+    content = f.readlines()
+
+print(content)
+
+# Show the file contents line by line.
+# We added the comma to print single newlines and not double newlines.
+# This is because the lines contain the newline character '\n'.
+for line in content:
+    print(line)
+
