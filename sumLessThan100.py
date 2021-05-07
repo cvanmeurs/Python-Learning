37a89beaba6b2f091cf67e97ef2f19d5f9441074
Christian Van Meurs <cvanmeurs@mozilla.com>
2021-05-06 16:30:22 -0800

    Generate random numbers until sum is greater than 100

--- sumLessThan100.py
+++ sumLessThan100.py
@@ -1,19 +1,19 @@
 import random
 
 numberList = []
-
 total = 0
 
 while total < 100:
-	randNum = random.randint(0, 10)
-		#if randNum = 
-	numberList.append(randNum)
+	randNum = random.randint(0, 10) #generate a random number between 0 and 10
+	numberList.append(randNum) #add the random number to a list called randNum
 	print("Generated number is: ", randNum)
-	total = total + randNum
+	total = total + randNum #add the generated number to the total
 
 
-#while total > 100:
-#	numberList = numberList[:-1]
+
+if total > 100:
+	numberList = numberList[:-1] #remove the last number if the total is greater than 100
+
 
 
 
