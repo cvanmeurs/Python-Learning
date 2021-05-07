7ddd365c7da16bd506656f1010be44b42ed6362b
Christian Van Meurs <cvanmeurs@mozilla.com>
2021-05-06 16:28:41 -0800

    updated file

--- sumLessThan100.py
+++ sumLessThan100.py
@@ -6,8 +6,14 @@ total = 0
 
 while total < 100:
 	randNum = random.randint(0, 10)
+		#if randNum = 
 	numberList.append(randNum)
+	print("Generated number is: ", randNum)
 	total = total + randNum
+
+
+#while total > 100:
+#	numberList = numberList[:-1]
 
 
 
