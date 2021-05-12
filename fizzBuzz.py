4dee7b8430aaab6fec0a2d98bcb4841fb12332a4
Christian Van Meurs <cvanmeurs@mozilla.com>
2021-05-12 14:33:37 -0800

    Adding completed FizzBuzz file

---
+++ fizzBuzz.py
@@ -0,0 +1,18 @@
+# Fizz Buzz!
+
+# Print all the numbers between 1 and 100
+# If the number is divisible by 3, print "Fizz"
+# If the number is divisible by 5, print "Buzz"
+# If the number is divisible by 3 AND 5, print "FizzBuzz"
+
+
+for x in range(1, 101):
+    if x%3 == 0 and x%5 == 0:
+    	print("FizzBuzz")
+    elif x%3 == 0:
+    	print("Fizz")
+    elif x%5 == 0:
+    	print("Buzz")
+    elif x%3 != 0 and x%5 != 0:
+    	print(x)
+
